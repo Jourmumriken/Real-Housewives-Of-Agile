7f3b162391532db3841162a10d54e294511083f4
O-Brob <broborn@student.chalmers.se>
2024-09-24 20:09:04 +0200

    Fixed bug where createSQLTable referenced a non-local variable as foreign key, and simplified table initialization via helper method + initialized account table.

--- JavaDatabase/src/DataAccessLayer.java
+++ JavaDatabase/src/DataAccessLayer.java
@@ -3,78 +3,102 @@ import java.sql.*;
 import java.util.ArrayList;
 
 /**
- * A class that provides an abstracted interface for accessing data from the database
+ * A class that provides an abstracted interface for accessing data from the
+ * database
  */
 public class DataAccessLayer {
     // *SET THIS TO TRUE ONLY IF YOU INTEND TO WIPE THE DATABASE ON NEXT RUN *//
     // Should be set to false outside of testing!
+    // Might need to run program a couple times with it as true to fully wipe
+    // Because they wipe one at a time due to constraint exceptions.
     static final boolean dropAllData = false;
 
     private final String createSQLTable = "CREATE TABLE RepairGuide (" +
             "id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, " +
             "title VARCHAR(255), " +
             "content VARCHAR(10000)," +
+            "username VARCHAR(15)," +
             "FOREIGN KEY (username) REFERENCES Account(username)" + // "ON DELETE CASCADE" maybe?
             ")";
-    private final String sqlAccount = "CREATE TABLE Account ("+
-            "username VARCHAR(15) PRIMARY KEY,"+ // better that we use an INT as primary key instead.
+    private final String sqlAccount = "CREATE TABLE Account (" +
+            "username VARCHAR(15) PRIMARY KEY," + // better that we use an INT as primary key instead.
             // But simple prototype first and incremental progress and all that.
-            "password VARCHAR(15) NOT NULL"+ // plaintext password LOL!
+            "password VARCHAR(15) NOT NULL" + // plaintext password LOL!
             ")";
 
+    /**
+     * Initializes and creates persistent data tables if
+     * they do not exist on the connection.
+     * 
+     * @param conn the connection object which the database will connect to.
+     */
     public DataAccessLayer(Connection conn) {
         if (conn != null) {
-            try {
-                // If dropAllData == true, delete the table (mainly to clean up testing)
-                if (dropAllData) {
-                    Statement statement = conn.createStatement();
-                    statement.executeUpdate("DROP TABLE RepairGuide");
-                    statement.close();
-                    System.out.println("RepairGuide table has been WIPED!");
-
-                } else { // Else create the table. If already existing -> SQLException
-                    Statement statement = conn.createStatement();
-                    statement.executeUpdate(createSQLTable);
-                    System.out.println("RepairGuide table has been created.");
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                // e.printStackTrace();
-                if (dropAllData)
-                    System.out.println("Can not delete table, it does not exist.");
-                else
-                    System.out.println("RepairGuide table already exists, using existing table...");
-            }
+            // Initialize each table without throwing exceptions.
+            // Note that Account must be initialized before RepairGuide,
+            // as RepairGuide has constraints to Account usernames.
+            initializeTable(conn, "Account", sqlAccount);
+            initializeTable(conn, "RepairGuide", createSQLTable);
         } else {
             throw new NullPointerException("Connection passed to DataAccessLayer was null.");
         }
     }
 
     /**
+     * Initializes a table based on the given SQL create statement.
+     * If dropAllData is true, the table will be dropped before creation.
+     * If the table already exists, it will catch the SQLException and continue.
+     * 
+     * @param conn      the database connection object
+     * @param tableName the name of the table to be initialized
+     * @param createSQL the SQL statement to create the table
+     */
+    private void initializeTable(Connection conn, String tableName, String createSQL) {
+        try (Statement statement = conn.createStatement()) {
+            if (dropAllData) {
+                statement.executeUpdate("DROP TABLE " + tableName);
+                System.out.println(tableName + " table has been WIPED!");
+            } else {
+                statement.executeUpdate(createSQL);
+                System.out.println(tableName + " table has been created.");
+            }
+        } catch (SQLException e) {
+            //System.out.println(e);
+            if (dropAllData) {
+                System.out.println("Cannot delete " + tableName + " table, it does not exist.");
+            } else {
+                System.out.println(tableName + " table already exists, using existing table...");
+            }
+        }
+    }
+
+    /**
      * returns a list of all accounts in db as Account objects
+     * 
      * @param conn the connection to the database to be queried
      * @return an ArrayList of Accounts objects
      * @throws SQLException throws if the query fails
      */
-    public ArrayList<Account> queryAllAccounts(Connection conn) throws SQLException{
+    public ArrayList<Account> queryAllAccounts(Connection conn) throws SQLException {
         String query = "SELECT username,password FROM Account";
         PreparedStatement stm = conn.prepareStatement(query);
         ResultSet rs = stm.executeQuery();
         ArrayList<Account> accounts = new ArrayList<>();
         while (rs.next()) {
-           String username= rs.getString("username");
-           String password= rs.getString("password");
-           Account a = new Account(username,password);
-           accounts.add(a);
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            Account a = new Account(username, password);
+            accounts.add(a);
         }
         return accounts;
     }
 
     /**
      * returns an Account object representing the account with the provided username
-     * @param conn the connection to the database to be queried
+     * 
+     * @param conn     the connection to the database to be queried
      * @param username the username of the account
-     * @return an Account object
+     * @return an Account object, or NULL if user doesn't exist!
      * @throws SQLException throws exception if the query fails
      */
     public Account queryAccount(Connection conn, String username) throws SQLException {
@@ -85,26 +109,28 @@ public class DataAccessLayer {
         Account a = null;
         while (rs.next()) {
             String password = rs.getString("password");
-            a = new Account(username,password);
+            a = new Account(username, password);
         }
         return a;
-}
+    }
 
     /**
      * Creates a new account in db with username and password in parameters
-     * @param conn the connection of the database to insert into
+     * 
+     * @param conn     the connection of the database to insert into
      * @param username the username
      * @param password the password
      * @throws SQLException throws exception if the insertion fails
      */
-    public void insertAccount(Connection conn, String username, String password) throws SQLException{
+    public void insertAccount(Connection conn, String username, String password) throws SQLException {
         String sql = "INSERT INTO Account (username,password) VALUES (?,?)";
         PreparedStatement stm = conn.prepareStatement(sql);
-        stm.setString(1,username);
-        stm.setString(2,password);
+        stm.setString(1, username);
+        stm.setString(2, password);
         stm.executeUpdate();
         stm.close();
-}
+    }
+
     /**
      * Returns a list of every guide in DB as Guide objects.
      * 
@@ -122,11 +148,11 @@ public class DataAccessLayer {
 
         // Go through results and add into the array as `Guides`
         while (rs.next()) {
-            int id =rs.getInt("id");
-           String title= rs.getString("title");
-            String content= rs.getString("content");
+            int id = rs.getInt("id");
+            String title = rs.getString("title");
+            String content = rs.getString("content");
             String username = rs.getString("username");
-            Guide g = new Guide(id,title,content,queryAccount(conn,username));
+            Guide g = new Guide(id, title, content, queryAccount(conn, username));
             guides.add(g);
         }
 
@@ -134,8 +160,8 @@ public class DataAccessLayer {
     }
 
     /**
-     * Creates new guide in db with title and content in parameters that is associated with the
-     * account in the parameters
+     * Creates new guide in db with title and content in parameters that is
+     * associated with the account in the parameters
      * 
      * @param conn    the connection as a Connection object to the database to
      *                insert into
@@ -144,12 +170,12 @@ public class DataAccessLayer {
      * @throws SQLException throws exception for various reasons, bad connection,
      *                      wrong data type, table does not exist and others
      */
-    public void insertGuide(Connection conn, String title, String content,Account account) throws SQLException {
+    public void insertGuide(Connection conn, String title, String content, Account account) throws SQLException {
         String sql = "INSERT INTO RepairGuide (title, content,username) VALUES (?, ?, ?)";
         PreparedStatement stm = conn.prepareStatement(sql);
         stm.setString(1, title);
         stm.setString(2, content);
-        stm.setString(3,account.getUsername()); // the username for the account that created the guide
+        stm.setString(3, account.getUsername()); // the username for the account that created the guide
         stm.executeUpdate();
         stm.close();
         // "INSERT INTO RepairGuide (title, content) VALUES (?,?,?)"
@@ -170,10 +196,10 @@ public class DataAccessLayer {
         ResultSet rs = stm.executeQuery();
         Guide g = null;
         while (rs.next()) {
-            String title= rs.getString("title");
-            String content= rs.getString("content");
+            String title = rs.getString("title");
+            String content = rs.getString("content");
             String username = rs.getString("username");
-            g = new Guide(id,title,content,queryAccount(conn,username)); // Each guide has an associated user
+            g = new Guide(id, title, content, queryAccount(conn, username)); // Each guide has an associated user
         }
         rs.close();
         stm.close();
@@ -197,9 +223,9 @@ public class DataAccessLayer {
         ArrayList<Guide> guides = new ArrayList<>();
         while (rs.next()) {
             int id = rs.getInt("id");
-            String content= rs.getString("content");
+            String content = rs.getString("content");
             String username = rs.getString("username");
-            Guide g = new Guide(id,title,content,queryAccount(conn,username));
+            Guide g = new Guide(id, title, content, queryAccount(conn, username));
             guides.add(g);
         }
         rs.close();
