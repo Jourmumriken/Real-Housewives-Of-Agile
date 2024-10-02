31bb75a1cee31ba0c09b6309da8b9e450fa0ae0d
idavvuk <ida.vranvuk@hotmail.com>
2024-10-02 23:18:00 +0200

    Did some more visual changes, added some icons

--- html/script.js
+++ html/script.js
@@ -1,7 +1,8 @@
-window.addEventListener("DOMContentLoaded", () => {
+
+/* window.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector(".toggle"); 
     const sideNavEl = document.querySelector(".side-nav"); 
-
+    const mainWithSide = document.querySelector(".")
     toggleButton.addEventListener("click", () => {
         sideNavEl.classList.toggle("side-nav--hidden"); 
         
@@ -12,3 +13,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 });
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.toggle'); // Select the toggle button
+    const sidebar = document.querySelector('.side-nav'); // Select the sidebar
+    const mainContainer = document.querySelector('.main-content-container'); // Select the main content container
+  
+    // Add event listener to the toggle button
+    toggleButton.addEventListener('click', function() {
+      // Toggle the hidden class on the sidebar
+      sidebar.classList.toggle('side-nav--hidden');
+      
+      // Toggle the sidebar-hidden class on the main content container
+    });
+  });
+  
+
+  
